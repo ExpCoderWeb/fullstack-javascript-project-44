5ce4c5c3c01687f305cb3d83c2ab7af8af879e95
ExpCoderWeb <shagaev_2011@mail.ru>
2024-11-16 22:55:15 +0400

    update src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const greetUser = () => {
+  console.log('Welcome to the Brain Games!');
   const username = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${username}!`);
+  return username;
 };
 export default greetUser;
