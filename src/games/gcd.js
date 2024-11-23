f79e2b739ca8b3872369ca647aab812f55bed255
ExpCoderWeb <shagaev_2011@mail.ru>
2024-11-23 13:41:17 +0400

    modify src/games/gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,43 +4,16 @@ import getRandomInRange from '../utils.js';
 const rules = 'Find the greatest common divisor of given numbers.';
 
 const findGreatestCommonDivider = (number1, number2) => {
-  if (number1 === 0 && number2 === 0) {
-    return 1;
-  }
-
-  if (number1 === number2) {
-    return number1;
-  }
-
-  if (number1 === 0) {
-    return number2;
-  }
-
-  if (number2 === 0) {
-    return number1;
-  }
-
   let a = number1;
   let b = number2;
 
-  if (number1 < number2) {
-    a = number2;
-    b = number1;
-  }
-
-  let remainder;
-  let GCD;
-
-  do {
-    remainder = a % b;
-    if (remainder === 0) {
-      GCD = b;
-    }
+  while (b !== 0) {
+    const remainder = a % b;
     a = b;
     b = remainder;
-  } while (remainder !== 0);
+  }
 
-  return GCD;
+  return a;
 };
 
 const makeGCDGame = () => {
